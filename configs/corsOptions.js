6c2b177f0e7ab57c33d4710b8a6f2f91af37c213
Tomisin Leshi <tom-lesh@outlook.com>
2025-07-28 18:44:46 +0100

    Add CORS options config and update dependencies
    Introduced a new (commented) CORS options configuration file at configs/corsOptions.js. Updated package.json and package-lock.json to include several new dependencies such as express, cors, mongoose, dotenv, and others, preparing the project for enhanced API and authentication features.

---
+++ configs/corsOptions.js
@@ -0,0 +1,16 @@
+// const whiteList = [
+//   "https://linkio.world",
+//   "https://admin.linkio.world",
+//   "https://app.linkio.world",
+//   "https://beta.linkio.world",
+//   "https://dev.linkio.world",
+//   "https://ramp.linkio.world",
+//   "http://localhost:3000",
+// ];
+
+// export const corsOptions = {
+//   origin: whiteList,
+//   credentials: true,
+//   methods: ["GET"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
